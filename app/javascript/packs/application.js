161c3d311fdef3285e4dca03a779d9f0edd8e2bf
Hiroki_T <gazette10@gmail.com>
2020-06-20 01:20:43 +0900

    halfway of refactor

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,14 +19,15 @@ require("bootstrap/dist/js/bootstrap")
 import Vue from 'vue/dist/vue.esm'
 import App from '../app.vue'
 
+window.store = {}
+
 document.addEventListener("turbolinks:load", function() {
   var element = document.querySelector("#boards")
   if (element != undefined) {
+    window.store.lists = JSON.parse(element.dataset.lists)
     const app = new Vue({
       el: element,
-      data: {
-        lists: JSON.parse(element.dataset.lists)
-      },
+      data: window.store,
       template: "<App :original_lists='lists' />",
       components: { App }
     })
